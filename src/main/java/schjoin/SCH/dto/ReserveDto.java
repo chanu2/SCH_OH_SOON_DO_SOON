81fc1b977d87cd1b6df24f119698e39b5bbe5b56
chanu2 <mdsoo55828@gmail.com>
2022-10-13 16:05:37 +0900

    API_TEST

--- src/main/java/schjoin/SCH/dto/ReserveDto.java
+++ src/main/java/schjoin/SCH/dto/ReserveDto.java
@@ -1,30 +1,45 @@
 package schjoin.SCH.dto;
 
-
 import lombok.Getter;
-import lombok.Setter;
+import schjoin.SCH.domain.Gender;
 import schjoin.SCH.domain.Sport;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
-@Setter
 public class ReserveDto {
-
-
 
     private Long id;
 
     private Sport sport;
 
-    private LocalDateTime reserveDate;
-    private LocalDateTime startT;
-    private LocalDateTime endT;
-
-    private Integer recruitmentNum;
+    private LocalDate reserveDate;
+    private LocalTime startT;
+    private LocalTime endT;
 
     private String title;
     private String explanation;
+    private Integer currentNum;
+    private Integer recruitmentNum;
 
 
+
+    private String place;
+
+    private Gender gender;
+
+    public ReserveDto(Long id, Sport sport, LocalDate reserveDate, LocalTime startT, LocalTime endT, String title, String explanation, Integer currentNum, Integer recruitmentNum, String place, Gender gender) {
+        this.id = id;
+        this.sport = sport;
+        this.reserveDate = reserveDate;
+        this.startT = startT;
+        this.endT = endT;
+        this.title = title;
+        this.explanation = explanation;
+        this.currentNum = currentNum;
+        this.recruitmentNum = recruitmentNum;
+        this.place = place;
+        this.gender = gender;
+    }
 }
